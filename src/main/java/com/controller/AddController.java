648c5abdb2d48e5d79e31c4ed37489063892e0a7
ed <e10002001-0@yahoo.com>
2020-05-14 15:12:36 +0200

    make it better

--- src/main/java/com/controller/AddController.java
+++ src/main/java/com/controller/AddController.java
@@ -1,30 +1,17 @@
 package com.controller;
 
-import java.sql.SQLException;
-import java.util.List;
-
 import javax.servlet.ServletRequest;
 
-import org.json.simple.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.Repository.JobRepository;
-import com.Repository.UserRepository;
-import com.Service.Auth;
-import com.Service.Data;
 import com.component.InitSessions;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.google.gson.Gson;
-import com.model.Job;
 import com.model.LoginSession;
-import com.model.User;
 
 
 @Controller
